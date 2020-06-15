aca2126d7eac9b9b2a839e484cc46a056cf47c36
MatteoMakovec <A!AScorpio98>
2020-06-15 12:55:27 +0200

    Working on computation requests

--- src/it/units/project/request/ComputationRequest.java
+++ src/it/units/project/request/ComputationRequest.java
@@ -7,8 +7,33 @@ public class ComputationRequest extends Request{
         super(regex, time);
     }
 
-    public String process(Collection<Request> requests, Request r){
+    public String process(Collection<Request> requests, Request r){     // Non servirebbe passare la Collection, ma devo per via della classe astratta
+        String result = inputProcessing(r.input);
+
+        return result;
+    }
+
+    private String inputProcessing(String input){
+        char computationSeparator = '_';
+        char separator = ';';
+        String workingString = input;
+
+        String computationKind;
+        String valuesKind;
+        String variableValues;
+        String expression;
+
+        int indexComputationSeparator = workingString.indexOf(computationSeparator);
+        computationKind = workingString.substring(0, indexComputationSeparator);
+        int indexSeparator = workingString.indexOf(separator);
+        valuesKind = workingString.substring(indexComputationSeparator+1, indexSeparator);
+        workingString = workingString.substring(indexSeparator+1);
+        indexSeparator = workingString.indexOf(separator);
+        variableValues = workingString.substring(0, indexSeparator);
+        expression = workingString.substring(indexSeparator+1);
+
         closeRequest(true);
-        return "OK";
+        String result = "Computation kind: " + computationKind + "\nValues kind: " + valuesKind + "\nVariable values kind: " + variableValues + "\nExpression: " + expression;
+        return result;
     }
 }

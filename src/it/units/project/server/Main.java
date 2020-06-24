8c3e9cd5d37322ffda97e7f81ae34efb60dabbfe
MatteoMakovec <A!AScorpio98>
2020-06-24 13:57:10 +0200

    Last changes...

--- src/it/units/project/server/Main.java
+++ src/it/units/project/server/Main.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
-            System.err.println("["+new Date()+"] " + "You should set the port number");
+            System.err.println("["+new Date()+"] " + "You must set the port number");
         }
         else{
             Server server = new ProcessingServer(Integer.valueOf(args[0]), "BYE");

3a9480bfbcd07291fcb1e146d0ee5808b3fa38ba
Mako04 <matteomako04@gmail.com>
2020-06-25 14:28:59 +0200

    Finished

--- src/it/units/project/request/StatRequest.java
+++ src/it/units/project/request/StatRequest.java
@@ -3,6 +3,8 @@ package it.units.project.request;
 import it.units.project.exception.CommandException;
 import it.units.project.response.SuccessfulResponse;
 
+import java.util.Locale;
+
 import static it.units.project.server.ProcessingServer.DECIMAL_PRECISION;
 
 
@@ -20,11 +22,11 @@ public class StatRequest extends AbstractRequest {
                 break;
 
             case "STAT_AVG_TIME":
-                result = new SuccessfulResponse(String.format("%." + DECIMAL_PRECISION + "f", (stats[1] / stats[0])), this).buildingResponse();
+                result = new SuccessfulResponse(String.format(Locale.US, "%." + DECIMAL_PRECISION + "f", (stats[1] / stats[0])), this).buildingResponse();
                 break;
 
             case "STAT_MAX_TIME":
-                result = new SuccessfulResponse(String.format("%." + DECIMAL_PRECISION + "f", stats[2]), this).buildingResponse();
+                result = new SuccessfulResponse(String.format(Locale.US, "%." + DECIMAL_PRECISION + "f", stats[2]), this).buildingResponse();
                 break;
 
             default:
